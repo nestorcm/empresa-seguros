68c16091f018d59ee208ee4525f2b9d4920dc8e7
nestor <nestor.mandon@gmail.com>
2023-09-08 21:45:04 -0500

    empieza ventas

---
+++ parcial/parcial/Controllers/VentasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace parcial.Controllers
+{
+    public class VentasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
